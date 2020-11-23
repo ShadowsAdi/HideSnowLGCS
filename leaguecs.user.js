cc67026278faa8731cdb83ce98fa93d68f160897
Adrian Cirstea <54354938+ShadowsAdi@users.noreply.github.com>
2020-11-23 21:42:33 +0200

    Create leaguecs.user.js

---
+++ leaguecs.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             Hide Snow
+// @name           Hide Snow
+// @version        1.0
+// @author         Shadows Adi
+// @include        https://leaguecs.ro/forum/
+// @run-at         document-end
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @homepageURL    https://github.com/ShadowsAdi/SnowHide
+// ==/UserScript==
+setInterval(function() {
+    $('.snow0').remove();
+}, 1000);
